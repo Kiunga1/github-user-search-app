4d175fd5feb50bacbfc064df41fe2207d0d9b5e6
ann5306 <annkiunga@gmail.com>
2024-02-19 10:29:09 +0300

    New changes

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ const App = () => {
 
 
   return (
-    <div className=' font-spaceMono w-100 h-screen bg-[#F6F8FF] dark:bg-primaryBlack '>
+    <div className=' font-spaceMono w-100 min-h-screen bg-[#F6F8FF] dark:bg-primaryBlack '>
       <div className='text-center lg:w-[40%] md:w-[50%] w-[90%] m-auto mt-5 flex flex-col gap-5'>
       <Header/>
       <SearchBar getGithubUser = {getGithubUser}/>
